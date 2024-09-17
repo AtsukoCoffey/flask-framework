cac9520ec0fbfc08b9d9634c2997c0516d0ae56e
Atsuko Coffey <atsucoffey@gmail.com>
2024-09-17 10:02:12 +0000

    feat: Create templates and pages

--- run.py
+++ run.py
@@ -10,6 +10,21 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/carrers")
+def carrers():
+    return render_template("carrers.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0,0,0,0"),
